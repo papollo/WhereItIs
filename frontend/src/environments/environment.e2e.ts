3fdca1023f4e142d9403c9d6a88fb0df9016d7c7
Piotr Apollo <piotr.apollo@q-perior.com>
2025-12-27 18:44:37 +0100

    Fix mistake

--- frontend/src/environments/environment.e2e.ts
+++ frontend/src/environments/environment.e2e.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://bkehywxkhctuqhourzsi.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZWh5d3hraGN0dXFob3VyenNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MTg3NjQsImV4cCI6MjA4MDE5NDc2NH0.Wr7w2fHZ0eUIm85IOby-C2417Mzi7GN_CvWmAeZkNHs',
+  supabaseUrl: '',
+  supabaseAnonKey: ''
 };
